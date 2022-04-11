5adddcf023a7e454c092dea609bc5cd2b789c5d0
ethanchang34 <ethanchang34@yahoo.com>
2022-04-11 10:58:16 -0400

    added PlanView component

---
+++ src/components/PlanView.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Plan } from "../interfaces/Plan";
+import { PlanEdit } from "./PlanEdit";
+
+export const PlanView = ({
+    plan,
+    addPlan,
+    deletePlan,
+    editPlan
+}: {
+    plan: Plan;
+    addPlan: () => void;
+    deletePlan: (id: number) => void;
+    editPlan: (id: number, newPlan: Plan) => void;
+}) => {
+    const [editing, setEditing] = useState<boolean>(false);
+
+    function changeEditing() {
+        setEditing(!editing);
+    }
+
+    return editing ? (
+        <PlanEdit
+            changeEditing={changeEditing}
+            plan={plan}
+            editPlan={editPlan}
+            deletePlan={deletePlan}
+        ></PlanEdit>
+    ) : (
+        <div>
+            <p>Id: {plan.id}</p>
+            <p>Title: {plan.title}</p>
+            <p>Description: {plan.description}</p>
+            <Button className="float-right" size="sm" onClick={changeEditing}>
+                Edit
+            </Button>
+        </div>
+    );
+};
