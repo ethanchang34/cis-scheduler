9afa6da29600afad3734d6e99562acc77de5d9e6
ethanchang34 <ethanchang34@yahoo.com>
2022-04-12 18:21:01 -0400

    implemented map of SemesterView

--- src/components/PlanView.tsx
+++ src/components/PlanView.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { Plan } from "../interfaces/Plan";
+import { Year } from "../interfaces/Year";
+import { Semester } from "../interfaces/Semester";
 import { PlanEdit } from "./PlanEdit";
+import { SemesterView } from "./SemesterView";
 
 export const PlanView = ({
     plan,
@@ -32,6 +35,21 @@ export const PlanView = ({
             <p>Id: {plan.id}</p>
             <p>Title: {plan.title}</p>
             <p>Description: {plan.description}</p>
+            {plan.years.map((year: Year) => (
+                <div
+                    key={plan.years.indexOf(year)}
+                    className="bg-light border m-2 p-2"
+                >
+                    {year.semesters.map((semester: Semester) => (
+                        <div
+                            key={year.semesters.indexOf(semester)}
+                            className="bg-light border m-2 p-2"
+                        >
+                            <SemesterView semester={semester}></SemesterView>
+                        </div>
+                    ))}
+                </div>
+            ))}
             <Button className="float-right" size="sm" onClick={changeEditing}>
                 Edit
             </Button>
