fe9a2de855abed52628ca58fad425f68b05e6b9f
ethanchang34 <ethanchang34@yahoo.com>
2022-04-16 17:07:51 -0400

    removed unecessary, vestigial code

--- src/components/SemesterView.tsx
+++ src/components/SemesterView.tsx
@@ -67,12 +67,6 @@ export const SemesterView = ({
                 deleteCourse={deleteCourse}
                 editCourse={editCourse}
             ></CourseList>
-            {semester.courses.map((course: Course) => (
-                <div
-                    key={course.code}
-                    className="bg-light border m-2 p-2"
-                ></div>
-            ))}
             <Button onClick={() => addCourse}>+ Add Course</Button>
             <Button onClick={clearCourses}>Clear Courses</Button>
             <Button onClick={() => deleteSemester(semester.id)}>
