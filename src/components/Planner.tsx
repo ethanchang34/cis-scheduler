c92208c85096f47095da8c62722d71db51a6e1f8
Colin <cstetler@udel.edu>
2022-04-18 10:24:45 -0400

    Removed unused import

--- src/components/Planner.tsx
+++ src/components/Planner.tsx
@@ -1,6 +1,5 @@
 import "../App.css";
 import React from "react";
-import { Button } from "react-bootstrap";
 import { Plan } from "../interfaces/Plan";
 import { Course } from "../interfaces/Course";
 import { PlanList } from "./PlanList";
