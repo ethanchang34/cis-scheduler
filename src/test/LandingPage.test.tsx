63dd4b4dee88dee909627ad38040430cbaed83f1
aidant <aidant@udel.edu>
2022-04-20 01:23:19 -0400

    added test folder and wrote test for some user stories, also changed around the nav bar to be more intuitive

---
+++ src/test/LandingPage.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { LandingPage } from "../components/LandingPage";
+
+const flipLanding = () => {};
+
+describe("Create landing page UI Students are introduced to the application with a friendly message that clearly explains their goal and how they should get started.", () => {
+    beforeEach(() => {
+        render(<LandingPage flipLanding={flipLanding} />);
+    });
+
+    test("renders the course name somewhere", () => {
+        const linkElement = screen.getByText(/CISC275/i);
+        expect(linkElement).toBeInTheDocument();
+    });
+
+    test("renders the title course", () => {
+        const linkElement = screen.getByText(/Course Schedule Planner/i);
+        expect(linkElement).toBeInTheDocument();
+    });
+
+    test("instructions for different personas appear on screen", () => {
+        const freshman = screen.getByText(/Freshman/i);
+        expect(freshman).toBeInTheDocument();
+
+        const sophomore = screen.getByText(/Sophomore/i);
+        expect(sophomore).toBeInTheDocument();
+
+        const junior = screen.getByText(/Junior/i);
+        expect(junior).toBeInTheDocument();
+    });
+
+    test("There is 1 button", () => {
+        const buttons = screen.getAllByRole("button");
+        expect(buttons.length).toBe(1);
+        expect(screen.getByText(/Get Started!/i)).toBeInTheDocument();
+    });
+});
