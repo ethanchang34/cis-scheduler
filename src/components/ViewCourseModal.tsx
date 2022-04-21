31710e01a644ebc4b799ba291aa259ae98cbf787
aidant <aidant@udel.edu>
2022-04-21 13:44:10 -0400

    moved modal content to separate component

--- src/components/ViewCourseModal.tsx
+++ src/components/ViewCourseModal.tsx
@@ -1,13 +1,7 @@
 import React /*{ useState }*/ from "react";
-import { Modal, Form } from "react-bootstrap";
-import styled from "styled-components";
+import { Modal } from "react-bootstrap";
 import { Course } from "../interfaces/Course";
-
-const FlexDiv = styled.div`
-    display: flex;
-    gap: 0.5rem;
-    margin-bottom: 0.25rem;
-`;
+import { CourseModalContent } from "./CourseModalContent";
 
 export const ViewCourseModal = ({
     show,
@@ -24,27 +18,6 @@ export const ViewCourseModal = ({
 }) => {
     const course = modifiedCourses[code];
 
-    let semsOffered = "";
-
-    if (course.semsOffered.length === 0) {
-        semsOffered = "No semesters specified";
-    } else {
-        for (let i = 0; i < course.semsOffered.length; i++) {
-            if (course.semsOffered[i] === 0) {
-                semsOffered = semsOffered + "Fall";
-            } else if (course.semsOffered[i] === 1) {
-                semsOffered = semsOffered + "Winter";
-            } else if (course.semsOffered[i] === 2) {
-                semsOffered = semsOffered + "Spring";
-            } else if (course.semsOffered[i] === 3) {
-                semsOffered = semsOffered + "Summer";
-            }
-            if (i != course.semsOffered.length - 1) {
-                semsOffered = semsOffered + ", ";
-            }
-        }
-    }
-
     const handleTech = () => {
         editCourse({ ...course, tech: !course.tech });
     };
@@ -57,43 +30,10 @@ export const ViewCourseModal = ({
                 animation={false}
                 dialogClassName="my-modal"
             >
-                <Modal.Header closeButton>
-                    <Modal.Title>{course.code + " " + course.name}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <b>Description:</b>
-                    <p>{course.descr + " " + course.restrict}</p>
-                    <FlexDiv>
-                        <b>Semesters Offered: </b>
-                        <span>{semsOffered}</span>
-                    </FlexDiv>
-                    <FlexDiv>
-                        <b>Credits:</b> <span>{course.credits}</span>
-                    </FlexDiv>
-                    <FlexDiv>
-                        <b>Prerequisites:</b>
-                        <span>{course.preReq ? course.preReq : "None"}</span>
-                    </FlexDiv>
-                    <FlexDiv>
-                        <b>Breadth: </b>
-                        <span>{course.breadth ? course.breadth : "None"}</span>
-                    </FlexDiv>
-                    <FlexDiv>
-                        <b>Tech: </b>
-                        <Form.Check
-                            type="checkbox"
-                            id="tech-check"
-                            name="tech-check"
-                            data-testid="tech-check"
-                            value="tech-check"
-                            checked={course.tech}
-                            onChange={handleTech}
-                        />
-                    </FlexDiv>
-                </Modal.Body>
-                <Modal.Footer>
-                    <h1>Modal Footer</h1>
-                </Modal.Footer>
+                <CourseModalContent
+                    course={course}
+                    handleTech={handleTech}
+                ></CourseModalContent>
             </Modal>
         </div>
     );
