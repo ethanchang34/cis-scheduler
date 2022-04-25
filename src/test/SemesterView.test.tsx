0e717326983c37b5b4a73ce65acd2885c1a4f57a
ethanchang34 <ethanchang34@yahoo.com>
2022-04-25 02:02:18 -0400

    added semesters tests

--- src/test/SemesterView.test.tsx
+++ src/test/SemesterView.test.tsx
@@ -45,3 +45,26 @@ describe("User should be able to add and delete semesters", () => {
         expect(newSems.length).toBe(oldSems.length - 1);
     });
 });
+
+describe("User should be able to clear all courses from a semester", () => {
+    beforeEach(() => {
+        render(
+            <SemesterView
+                semester={DefaultPlans[0].years[0].semesters[0]}
+                deleteSemester={deleteSemester}
+                editSemester={editSemester}
+                modifiedCourses={testCourses}
+            />
+        );
+    });
+
+    test("You can clear all courses in a semester", () => {
+        const clearCourses = screen.getByRole("button", {
+            name: /clear courses/i
+        });
+        expect(clearCourses).toBeInTheDocument();
+        clearCourses.click();
+        const credits = screen.getAllByText(/credits/i); //idk a better way to grab the number of courses on the screen
+        expect(credits.length).toBe(0);
+    });
+});
