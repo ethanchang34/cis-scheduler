ed6af441cefe7b35b11382e198a043b710b55553
aidant <aidant@udel.edu>
2022-04-26 20:07:45 -0400

    fixed linter error

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { cleanup, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
 import { MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
