fbd1dbe535f57f2c1932098db7b711e46270e60e
ethanchang34 <ethanchang34@yahoo.com>
2022-04-30 18:07:51 -0400

    added auth0 wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-//import App from "./App";
+import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { LoginPage } from "./LoginPage";
 import * as uri from "./uri";
@@ -18,7 +18,7 @@ ReactDOM.render(
                 clientId="sI0gFY8VJTI1guaDRmSsadvUKZNr4K37"
                 redirectUri={uri.AUTH0_REDIRECT_URI}
             >
-                <LoginPage />
+                <App />
             </Auth0Provider>
         </BrowserRouter>
     </React.StrictMode>,
