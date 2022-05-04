edf49b1593a847699a0242cf40b4fdecf8cdd4cb
ethanchang34 <ethanchang34@yahoo.com>
2022-05-04 02:13:40 -0400

    working on uploading user metadata

--- src/login_components/UserMetaData.tsx
+++ src/login_components/UserMetaData.tsx
@@ -5,6 +5,7 @@ export const UserMetaData = () => {
     const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
     const [userMetadata, setUserMetadata] = useState<string | null>(null);
 
+    //reading metadata?
     useEffect(() => {
         const getUserMetadata = async () => {
             const domain = "dev--0t6-2tu.us.auth0.com";
@@ -39,6 +40,44 @@ export const UserMetaData = () => {
 
         getUserMetadata();
     }, [getAccessTokenSilently, user?.sub]);
+
+    //updating metadata?
+    useEffect(() => {
+        const setMetadata = async () => {
+            const domain = "dev--0t6-2tu.us.auth0.com";
+
+            if (user && isAuthenticated) {
+                try {
+                    const accessToken = await getAccessTokenSilently({
+                        audience: `https://${domain}/api/v2/`,
+                        scope: "read:current_user update:current_user_metadata"
+                    });
+
+                    const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+                    await fetch(userDetailsByIdUrl, {
+                        method: "PATCH",
+                        body: JSON.stringify(userMetadata),
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                            "content-type": "application/json"
+                        }
+                    });
+
+                    // const { user_metadata } = await metadataResponse.json();
+
+                    // setUserMetadata(user_metadata);
+                } catch (e) {
+                    if (typeof e === "string") {
+                        console.log(e.toUpperCase()); // works, `e` narrowed to string
+                    } else if (e instanceof Error) {
+                        console.log(e.message); // works, `e` narrowed to Error
+                    }
+                }
+            }
+        };
+        setMetadata();
+    }, [userMetadata]);
 
     const handle = () => {
         setUserMetadata(localStorage.getItem("CISC275-4-plans"));
